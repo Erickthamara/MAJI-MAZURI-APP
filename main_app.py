2aff709dab47dde546ab9ddc2324d581ea45f503
Erickthamara <105923301+Erickthamara@users.noreply.github.com>
2023-05-04 21:00:11 +0300

    Autentication
    Added label below each MDTextField that displays information to the user incase of wrong or no input at all. Did all the functionality in the .py file mainly using if...elif.....else statements.

--- main_app.py
+++ main_app.py
@@ -10,10 +10,116 @@ Window.size=(350,600)
 
 class WelcomeScreen(Screen): 
     pass
+
+
 class LoginScreen(Screen): 
-    pass
+    #This are all the methods used in the Login screen backend
+    def validate_email(self):
+        #Here we grab the data inputed using the .strip() function
+        email=self.ids.email_login.text.strip()
+        
+        if not email:
+            self.ids.email_login_error.text="Email is Required"
+            self.ids.signin_button.disabled=True
+        elif '@' not in email or '.' not in email:
+            self.ids.email_login_error.text="Invalid Email"
+            self.ids.signin_button.disabled=True
+        else:
+            self.ids.email_login_error.text=""
+            self.ids.signin_button.disabled=False
+
+    def validate_password1(self):
+        #Here we grab the data inputed using the .strip() functio
+        password1=self.ids.pswd1.text.strip()
+        if not password1:
+            self.ids.pswd1_error.text="Password is Required"
+            self.ids.signin_button.disabled=True
+        elif len(self.ids.pswd1.text) <4:
+            self.ids.pswd1_error.text="Password Too Short" 
+            self.ids.signin_button.disabled=True
+        else:
+            self.ids.pswd1_error.text=""
+            self.ids.signin_button.disabled=False
+
+
 class SignupScreen(Screen):
-    pass
+     #This are all the methods used in the signup screen backend
+     def validate_email2(self):
+        #Here we grab the data inputed using the .strip() function
+        email=self.ids.email_signup.text.strip()
+        
+        if not email:
+            self.ids.email_signup_error.text="Email is Required"
+        elif '@' not in email or '.' not in email:
+            self.ids.email_signup_error.text="Invalid Email"
+        else:
+            self.ids.email_signup_error.text=""
+
+     def validate_phone_no(self):
+        #Here we grab the data inputed using the .strip() function
+        phone_no=self.ids.phone_no.text.strip()
+        
+        if not phone_no:
+            self.ids.phone_no_error.text="Phone Number Required"
+        elif self.ids.phone_no.text.isdigit()==False  :
+            self.ids.phone_no_error.text="Invalid Phone Number"
+        else:
+            self.ids.phone_no_error.text=""
+
+     def validate_first_name(self):
+        #Here we grab the data inputed using the .strip() function
+        first_name=self.ids.first_name.text.strip()
+        
+        if not first_name:
+            self.ids.first_name_error.text="Input Required"
+        elif self.ids.first_name.text.isalpha()==False:
+            self.ids.first_name_error.text="Incorrect Input"
+        else:
+            self.ids.first_name_error.text=""
+
+     def validate_last_name(self):
+        #Here we grab the data inputed using the .strip() function
+        last_name=self.ids.last_name.text.strip()
+        
+        if not last_name:
+            self.ids.last_name_error.text="Input Required"
+        elif self.ids.last_name.text.isalpha()==False:
+            self.ids.last_name_error.text="Incorrect Input"
+        else:
+            self.ids.last_name_error.text=""
+
+     def validate_password2(self):
+        #Here we grab the data inputed using the .strip() function
+        password2=self.ids.pswd_signup.text.strip()
+        if not password2:
+            self.ids.pswd_signup_error.text="Password is Required"
+        elif len(self.ids.pswd_signup.text) <4:
+            self.ids.pswd_signup_error.text="Passowrd Too Short" 
+        else:
+            self.ids.pswd_signup_error.text=""
+
+     def validate_password3(self):
+        #Here we grab the data inputed using the .strip() function
+        password3=self.ids.pswd_confirm.text.strip()
+        if not password3:
+            self.ids.pswd_confirm_error.text="Password is Required"
+        elif len(self.ids.pswd_confirm.text) <4:
+            self.ids.pswd_confirm_error.text="Passowrd Too Short" 
+        elif self.ids.pswd_signup.text!=self.ids.pswd_confirm.text:
+            self.ids.pswd_confirm_error.text="Passwords Do Not Match"
+        else:
+            self.ids.pswd_confirm_error.text=""
+
+     def checkbox_validate(self):
+         if  not self.ids.checkbox2.active and not self.ids.checkbox3.active:
+             self.ids.checkbox_error.text="Select Customer or Seller"
+         else:
+             self.ids.checkbox_error.text=""
+
+         if self.ids.checkbox2.active:
+             print("You chose Customer!")
+         elif self.ids.checkbox3.active:
+             print("You chose seller")
 
 
 sm=ScreenManager()
