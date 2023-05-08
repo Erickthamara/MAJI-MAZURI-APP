dbdbda78e739d24ded3f02c6519fba72492bbda2
Erickthamara <105923301+Erickthamara@users.noreply.github.com>
2023-05-08 16:00:19 +0300

    Updated validation and arrow
    I updated the validation on the sign-up page by introducind a new method (checkbox_check) to disable the sign up page if the user had not chsen one.Also added a back button for the user to go back to the sign in page in ace they wanted to.

--- main_app.py
+++ main_app.py
@@ -71,8 +71,7 @@ class LoginScreen(Screen,Database):
         elif not password1 :
             self.ids.signin_button.disabled=True
             self.ids.pswd1_error.text="Password is Required"
-        else:
-            
+        else:   
             self.ids.signin_button.disabled=False
         
         self.cursor.execute("SELECT * FROM word;")
@@ -88,28 +87,26 @@ class LoginScreen(Screen,Database):
                     print("INCORRECT PASSWORD")
         else:
             print("INCORRECT EMAIL")
-
-
-        
+     
     # def next_page(self):
     #     print("New Page")
 
-        
-            
-
-
 class SignupScreen(Screen,Database):
      #This are all the methods used in the signup screen backend
+     
      def validate_email2(self):
         #Here we grab the data inputed using the .strip() function
         email=self.ids.email_signup.text.strip()
         
         if not email:
             self.ids.email_signup_error.text="Email is Required"
+            self.ids.submit_button2.disabled=True
         elif '@' not in email or '.' not in email:
             self.ids.email_signup_error.text="Invalid Email"
+            self.ids.submit_button2.disabled=True
         else:
             self.ids.email_signup_error.text=""
+            self.ids.submit_button2.disabled=False
 
         
 
@@ -119,10 +116,13 @@ class SignupScreen(Screen,Database):
         
         if not phone_no:
             self.ids.phone_no_error.text="Phone Number Required"
+            self.ids.submit_button2.disabled=True
         elif self.ids.phone_no.text.isdigit()==False  :
             self.ids.phone_no_error.text="Invalid Phone Number"
+            self.ids.submit_button2.disabled=True
         else:
             self.ids.phone_no_error.text=""
+            self.ids.submit_button2.disabled=False
 
      def validate_first_name(self):
         #Here we grab the data inputed using the .strip() function
@@ -130,10 +130,13 @@ class SignupScreen(Screen,Database):
         
         if not first_name:
             self.ids.first_name_error.text="Input Required"
+            self.ids.submit_button2.disabled=True
         elif self.ids.first_name.text.isalpha()==False:
             self.ids.first_name_error.text="Incorrect Input"
+            self.ids.submit_button2.disabled=True
         else:
             self.ids.first_name_error.text=""
+            self.ids.submit_button2.disabled=False
 
      def validate_last_name(self):
         #Here we grab the data inputed using the .strip() function
@@ -141,47 +144,96 @@ class SignupScreen(Screen,Database):
         
         if not last_name:
             self.ids.last_name_error.text="Input Required"
+            self.ids.submit_button2.disabled=True
         elif self.ids.last_name.text.isalpha()==False:
             self.ids.last_name_error.text="Incorrect Input"
+            self.ids.submit_button2.disabled=True
         else:
             self.ids.last_name_error.text=""
+            self.ids.submit_button2.disabled=False
 
      def validate_password2(self):
         #Here we grab the data inputed using the .strip() function
         password2=self.ids.pswd_signup.text.strip()
         if not password2:
             self.ids.pswd_signup_error.text="Password is Required"
+            self.ids.submit_button2.disabled=True
         elif len(self.ids.pswd_signup.text) <4:
             self.ids.pswd_signup_error.text="Passowrd Too Short" 
+            self.ids.submit_button2.disabled=True
         else:
             self.ids.pswd_signup_error.text=""
+            self.ids.submit_button2.disabled=False
+
 
      def validate_password3(self):
         #Here we grab the data inputed using the .strip() function
         password3=self.ids.pswd_confirm.text.strip()
         if not password3:
             self.ids.pswd_confirm_error.text="Password is Required"
+            self.ids.submit_button2.disabled=True
         elif len(self.ids.pswd_confirm.text) <4:
             self.ids.pswd_confirm_error.text="Passowrd Too Short" 
+            self.ids.submit_button2.disabled=True
         elif self.ids.pswd_signup.text!=self.ids.pswd_confirm.text:
             self.ids.pswd_confirm_error.text="Passwords Do Not Match"
+            self.ids.submit_button2.disabled=True
         else:
             self.ids.pswd_confirm_error.text=""
+            self.ids.submit_button2.disabled=False
 
-     def checkbox_validate(self):
-         if  not self.ids.checkbox2.active and not self.ids.checkbox3.active:
-             self.ids.checkbox_error.text="Select Customer or Seller"
-         else:
-             self.ids.checkbox_error.text=""
-
+     def checkbox_checked (self):
+         
+        if self.ids.checkbox2.active or self.ids.checkbox3.active: 
+            self.ids.checkbox_error.text=" "
+            self.ids.submit_button2.disabled=False
+        else: 
+            self.ids.checkbox_error.text="Select Customer or Seller"
+            self.ids.submit_button2.disabled=True
          
 
+     def checkbox_validate(self):
+         
+         email=self.ids.email_signup.text.strip()
+         phone_no=self.ids.phone_no.text.strip()
+         first_name=self.ids.first_name.text.strip()
+         last_name=self.ids.last_name.text.strip()
+         password2=self.ids.pswd_signup.text.strip()
+         password3=self.ids.pswd_confirm.text.strip()
+
+         if not email:
+             self.ids.submit_button2.disabled=True
+             self.ids.email_signup_error.text="Email is Required"
+         elif not phone_no:
+             self.ids.submit_button2.disabled=True
+             self.ids.phone_no_error.text="Phone Number Required"
+         elif not first_name:
+             self.ids.submit_button2.disabled=True
+             self.ids.first_name_error.text="Input Required"
+         elif not last_name:
+             self.ids.submit_button2.disabled=True
+             self.ids.last_name_error.text="Input Required"
+         elif not password2:
+             self.ids.submit_button2.disabled=True
+             self.ids.pswd_signup_error.text="Password is Required"
+         elif not password3:
+             self.ids.submit_button2.disabled=True
+             self.ids.pswd_confirm_error.text="Password is Required"
+         elif  not self.ids.checkbox2.active and not self.ids.checkbox3.active:
+            self.ids.checkbox_error.text="Select Customer or Seller"
+            self.ids.submit_button2.disabled=True
+         else:
+             self.ids.submit_button2.disabled=False
+             
+        
          if self.ids.checkbox2.active:
-             print("You chose Customer!")
+            print("Customer Selected")
          elif self.ids.checkbox3.active:
-             print("You chose seller")
+            print("Seller Selected")
 
+           
 
+#Creation of every screen of the MAJI MAZURI APP
 sm=ScreenManager()
 sm.add_widget(WelcomeScreen(name="welcome"))
 sm.add_widget(LoginScreen(name="login"))
