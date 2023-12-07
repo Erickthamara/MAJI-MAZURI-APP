671a37aa533fdf4d000b39e1bd47132f56ea8df2
Erick Thamara <105923301+Erickthamara@users.noreply.github.com>
2023-12-07 10:56:17 +0300

    Update zmpesa_failed.py
    Took out personal info

--- classes/zmpesa_failed.py
+++ classes/zmpesa_failed.py
@@ -10,14 +10,14 @@ from requests.auth import HTTPBasicAuth
 from urllib.parse import parse_qs
 import base64
 
-consumer_key = 'U3uZndIeO10CSUAyjahb6uHTTnTTA9Tx'
-consumer_secrete = 'lgJH0xs5hGoz5JE7'
-lnm_passkey ="bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+consumer_key = ''
+consumer_secrete = ''
+lnm_passkey =""
 bs_shortcode ="174379" # paybill number
 acess_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
 b2c_initiator = 'testapi' # initiator name
-mon="asxasxasdashdaschabachdbcdhcsdcdccsdcsd"
-mon2="ascasbcascbascjhcbajhcbasxchbascbas"
+mon=""
+mon2=""
 class MpesaClient:
     def format_time(self):
         unformatted_datetime = datetime.now()
@@ -188,4 +188,4 @@ def mpesa_call(phone_number,pay_amount):
 
 
 
-#mpesa_call("254796892684",1)
+#mpesa_call("",1)
